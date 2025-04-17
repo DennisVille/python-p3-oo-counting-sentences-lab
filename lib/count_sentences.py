4a144e377cd351d08031c5d62f9bb9d4d6b4dcef
dennis mukuria <denismukuria@gmail.com>
2025-04-17 03:10:11 +0300

    complete all tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  def __init__(self, value = ''):
+    self.value = value
+
+  @property
+  def value(self):
+    """return value property"""
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print('The value must be a string.')
+
+  def is_sentence(self):
+    if self.value[len(self.value) - 1] == '.':
+      return True
+    else: return False
+
+  def is_question(self):
+    if self.value[len(self.value) - 1] == '?':
+      return True
+    else: return False
+
+  def is_exclamation(self):
+    if self.value[len(self.value) - 1] == '!':
+      return True
+    else: return False
+
+  def count_sentences(self):
+    self.str_replace = self.value
+    for i in range(len(self.value)):
+      if self.value[i] in '?.!' and self.value[i] == self.value[i - 1]:
+        self.str_replace.replace(self.str_replace[i], ',', 1)
+    str_list = [s for s in re.split(r'[.?!]', self.str_replace) if s]
+    return len(str_list)
